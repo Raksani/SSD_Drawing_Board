cb9eed572d8f77b6708a9d017baca82a67eaad07
Raksani <raksani.k@ku.th>
2020-04-08 23:01:08 +0700

    finish my work part in CompositeObject.java

--- src/objects/CompositeGObject.java
+++ src/objects/CompositeGObject.java
@@ -7,42 +7,53 @@ import java.util.List;
 
 public class CompositeGObject extends GObject {
 
-	private List<GObject> gObjects;
+    private List<GObject> gObjects;
 
-	public CompositeGObject() {
-		super(0, 0, 0, 0);
-		gObjects = new ArrayList<GObject>();
-	}
+    public CompositeGObject() {
+        super(0, 0, 0, 0);
+        gObjects = new ArrayList<GObject>();
+    }
 
-	//add and remove Karn part
-	public void add(GObject gObject) {
-		// TODO: Implement this method.
-	}
+    //add and remove Karn part
+    public void add(GObject gObject) {
+        // TODO: Implement this method.
+    }
 
-	public void remove(GObject gObject) {
-		// TODO: Implement this method.
-	}
+    public void remove(GObject gObject) {
+        // TODO: Implement this method.
+    }
 
-	// Beau Part
-	@Override
-	public void move(int dX, int dY) {
-		// TODO: Implement this method.
-	}
+    // Beau Part
+    @Override
+    public void move(int dX, int dY) {
+        //move all objects in the list
+        for (GObject gObject : gObjects) {
+//			gObject.x += dX;
+//			gObject.y += dY;
+            gObject.move(dX, dY);
+        }
+    }
 
-	//Karn Part
-	public void recalculateRegion() {
-		// TODO: Implement this method.
-	}
+    //Karn Part
+    public void recalculateRegion() {
+        // TODO: Implement this method.
+    }
 
-	//Beau Part
-	@Override
-	public void paintObject(Graphics g) {
-		// TODO: Implement this method.
-	}
+    //Beau Part
+    @Override
+    public void paintObject(Graphics g) {
+        // paint all objects, both oval and rectangle by overriding their own method.
+        for (GObject gObject : gObjects) {
+            gObject.paintObject(g);
+        }
+    }
 
-	@Override
-	public void paintLabel(Graphics g) {
-		// TODO: Implement this method.
-	}
-	
+    @Override
+    public void paintLabel(Graphics g) {
+        // paint all objects, both oval and rectangle by overriding their own method.
+        for (GObject gObject : gObjects) {
+            gObject.paintLabel(g);
+        }
+    }
+
 }
